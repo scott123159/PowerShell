4e0dd1e7d266fd75e2d1bb0ae6a353bbdbafa0de
You-Hua Liao <r11631041@ntu.edu.tw>
2024-03-20 08:56:40 +0800

    add poweroff and restart function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 Import-Module -Name Terminal-Icons
+ipconfig /flushdns > $null
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tonybaloney.omp.json" | Invoke-Expression
 Set-Alias vim nvim
 Set-Alias ll Get-ChildItem
 $NvimDirectory = "$env:LOCALAPPDATA\nvim\"
 function tree { tree.com /f /a}
+function poweroff { shutdown /s /t 0 }
+function reboot { shutdown /r /t 0 }

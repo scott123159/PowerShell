ac737a46efd498b7b52cabed38e7cc6a6b7e8a66
You-Hua Liao <r11631041@ntu.edu.tw>
2024-03-22 19:05:39 +0800

    add remove nvim-data folder function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-Import-Module -Name Terminal-Icons
 ipconfig /flushdns > $null
+Import-Module -Name Terminal-Icons
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tonybaloney.omp.json" | Invoke-Expression
 Set-Alias vim nvim
 Set-Alias ll Get-ChildItem
@@ -7,3 +7,9 @@ $NvimDirectory = "$env:LOCALAPPDATA\nvim\"
 function tree { tree.com /f /a}
 function poweroff { shutdown /s /t 0 }
 function reboot { shutdown /r /t 0 }
+function rmnvim {
+  $NvimDataDirectory = "$env:LOCALAPPDATA\nvim-data\"
+  if (Test-Path $NvimDirectory) {
+    Remove-Item -Recurse $NvimDataDirectory
+  }
+}

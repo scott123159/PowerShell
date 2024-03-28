908329783537ec0afcd02a08196e86e0ec18f3ba
You-Hua Liao <r11631041@ntu.edu.tw>
2024-03-28 17:34:26 +0800

    add /f option to poweroff and reastart function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,20 @@
 ipconfig /flushdns > $null
+
 Import-Module -Name Terminal-Icons
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tonybaloney.omp.json" | Invoke-Expression
+
 Set-Alias vim nvim
 Set-Alias ll Get-ChildItem
+
 $NvimDirectory = "$env:LOCALAPPDATA\nvim\"
+
 function tree { tree.com /f /a}
-function poweroff { shutdown /s /t 0 }
-function reboot { shutdown /r /t 0 }
+function poweroff { shutdown /f /s /t 00 }
+function reboot { shutdown /f /r /t 00 }
 function rmnvim {
   $NvimDataDirectory = "$env:LOCALAPPDATA\nvim-data\"
   if (Test-Path $NvimDataDirectory) {
-    Remove-Item -Recurse $NvimDataDirectory
+    Remove-Item -Recurse -Force $NvimDataDirectory
   }
 }

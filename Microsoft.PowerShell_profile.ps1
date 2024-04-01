46de05d1309a53c8499046b315d88b7033750728
You-Hua Liao <r11631041@ntu.edu.tw>
2024-04-01 10:56:28 +0800

    add EDITRO environment variable

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,13 @@
-ipconfig /flushdns > $null
-
 Import-Module -Name Terminal-Icons
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tonybaloney.omp.json" | Invoke-Expression
+ipconfig /flushdns > $null
 
 Set-Alias vim nvim
 Set-Alias ll Get-ChildItem
 
 $NvimDirectory = "$env:LOCALAPPDATA\nvim\"
+$env:EDITOR = "nvim"
 
 function tree { tree.com /f /a}
 function poweroff { shutdown /f /s /t 00 }
